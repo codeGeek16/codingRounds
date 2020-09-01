1f0346bb7c90f973efe0be7c1c44c45015adc28c
Sahil Sood <sahil.aieee@gmail.com>
2020-09-01 07:17:14 +0530

    added controllers to accept unit price,offers and basket

--- src/main/java/com/example/promotionEngine/controller/PromotionController.java
+++ src/main/java/com/example/promotionEngine/controller/PromotionController.java
@@ -1,10 +1,38 @@
 package com.example.promotionEngine.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.promotionEngine.dto.SkuBasket;
+import com.example.promotionEngine.dto.SkuOffers;
+import com.example.promotionEngine.dto.SkuUnitPrice;
+import com.example.promotionEngine.service.PromotionService;
+
 @RestController
+@RequestMapping(value="/api/promotion")
 public class PromotionController {
 
+	@Autowired
+	private PromotionService promotionService;
 	
+	@PostMapping(value ="/basket")
+	public void computeBasket(@RequestBody SkuBasket basket){
+		promotionService.computeBasket(basket);
+	}
+	
+	@PostMapping(value ="/unitPriceList")
+	public void skuUnitPrice(@RequestBody List<SkuUnitPrice> unitPriceList){
+		promotionService.skuUnitPrice(unitPriceList);
+	}
+	
+	@PostMapping(value ="/offersList")
+	public void skuOfferPrice(@RequestBody List<SkuOffers> offerPriceList){
+		promotionService.skuOfferPrice(offerPriceList);
+	}
 	
 }
--- src/main/java/com/example/promotionEngine/service/PromotionService.java
+++ src/main/java/com/example/promotionEngine/service/PromotionService.java
@@ -1,8 +1,29 @@
 package com.example.promotionEngine.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.example.promotionEngine.dto.SkuBasket;
+import com.example.promotionEngine.dto.SkuOffers;
+import com.example.promotionEngine.dto.SkuUnitPrice;
 
 @Service
 public class PromotionService {
+
+	public void computeBasket(SkuBasket basket) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void skuUnitPrice(List<SkuUnitPrice> unitPriceList) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void skuOfferPrice(List<SkuOffers> offerPriceList) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
